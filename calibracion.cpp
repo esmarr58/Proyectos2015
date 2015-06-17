3449596f5f77e6ba0d4baf8e5a3ce7f7403b5704
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-06-17 16:24:37 -0500

    Update calibracion.cpp

--- calibracion.cpp
+++ calibracion.cpp
@@ -35,6 +35,15 @@
 using namespace cv;
 using namespace std;
 
+int LadoA=0;
+	int LadoB=0;
+	int NoDivisiones=0;
+	int DistanciaX = 0;
+	int DistanciaY = 0;
+	int i=0;
+	int j=0;
+	int inc = 0;
+
 static void SiRatonMueve(int evt, int x, int y, int flags, void* param){
 	switch(evt)
 	{
@@ -54,38 +63,66 @@ int main( void ){
 		tamanio 600x400, 8 bits
 	
 	*/
-	Mat ventana_calibracion(	500,
-														600,
+	
+
+	cout << "Indicar cuanto mide el lado A: ";
+	cin >> LadoA;
+
+	cout << "Indicar cuanto mide el lado B: ";
+	cin >> LadoB;
+
+	cout << "Indicar cuantos circulos por lado ";
+	cin >> NoDivisiones;
+	int Punto_Y[NoDivisiones*NoDivisiones-1];
+	int Punto_X[NoDivisiones*NoDivisiones-1];
+	
+	DistanciaX = LadoA/(NoDivisiones+1);
+	DistanciaY = LadoB/(NoDivisiones+1);
+
+	cout << DistanciaX << endl;
+	cout << DistanciaY << endl;
+
+	Mat ventana_calibracion(	LadoB,
+														LadoA,
 														CV_8UC3,
 														Scalar(255,255,255));
+
+	for(i = 1; i <= NoDivisiones; i++){
+			for(j = 1; j<= NoDivisiones; j++){
+				 Punto_Y[inc]    = DistanciaY*j;
+				 Punto_X[inc]	   = DistanciaX*i;
+					inc++;
+				//cout << DistanciaX*i << endl;
+ 			}
+		
+	}
+
+
+
+
 	/*
 		Ciclo principal
 	*/
 	for(;;){
 			//Dibujar Circulo
 
-			int i = 1;
-			for(int x = 40; x <440; x=x+20){
-				cout  << "Valor de intentos" 
-							<< i << endl;
-							i++;
-				int ii=1;
-				for(int y = 40; y<440; y=y+20){
-						cout  << "Valor de intentos22: " 
-							<< ii << endl;
-							ii++;
+		for(i=0; i<=(NoDivisiones*NoDivisiones)-1; i++){
+			
 						circle(	ventana_calibracion,
-								Point(x,y),
-								5,
+								Point(Punto_X[i],Punto_Y[i]),
+								2,
 								Scalar(0,255,0),
 								CV_FILLED,
-								8,
+								2,
 								0);
 
+	}
+			
 
-				}
+
 				
-			}
+				
+			
 			
 
 
